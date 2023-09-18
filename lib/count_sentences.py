423153309b614213ebd2fe284e8c666463934dcd
Kamyar Taghavi <caracicatriz.kt@gmail.com>
2023-09-17 22:38:01 -0600

    completed lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__ (self, value=""):
+    self._value = value
+
+  @property
+  def value (self):
+    return self._value
+
+  @value.setter
+  def value (self, stringVal):
+    if(type(stringVal) == str):
+      self._value = stringVal
+    else: 
+      print("The value must be a string.")
+    
+  def is_sentence(self):
+    return self._value.endswith(".")
+
+  def is_question(self):
+    return self._value.endswith("?")
+
+  def is_exclamation(self):
+    return self._value.endswith("!")
+  
+  def count_sentences (self):
+    value = self.value
+    for punc in ['!', '?']:
+      value = value.replace(punc, '.')
+
+    sentences = [s for s in value.split('.') if s]
+    return len(sentences)
